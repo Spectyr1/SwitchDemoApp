81cf8691a459f482c469db3c40dbd6d0abe3acf6
Danny Warren <danny@theforbiddenarts.com>
2022-03-23 15:46:39 -0400

    upload code

--- SwitchDemo/Program.cs
+++ SwitchDemo/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string firstName = "Sue";
+int age = 189;
+
+switch (age)
+{
+    case >= 0 and < 18:
+        Console.WriteLine("You are a child.");
+        break;
+    case >= 18 and < 66:
+        Console.WriteLine("You should have a job.");
+        break;
+    case >= 66:
+        Console.WriteLine("Hopefully you are retired or retiring soon.");
+        break;
+    default:
+        Console.WriteLine("Age was not in expected range.");
+        break;
+}
+
+//switch (firstName.ToLower())
+//{
+//    case "tim" or "sue":
+//        Console.WriteLine("Hello Professor.");
+//        break;
+//    case "tom":
+//        Console.WriteLine("Hello Tom.");
+//        break;
+//    default:
+//        Console.WriteLine("I don't know you.");
+//        break;
+//}
+
